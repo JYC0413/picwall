20da44ee6e8e64497be14cb511b4daf6cdcc4507
Zhe Zhang <zhangzhe0177@gmail.com>
2020-12-21 02:13:11 +0800

    提交图片时记录sessionId

--- src/main/kotlin/net/asec01/bcu/qingnian/picwall/ApiController.kt
+++ src/main/kotlin/net/asec01/bcu/qingnian/picwall/ApiController.kt
@@ -3,6 +3,7 @@ package net.asec01.bcu.qingnian.picwall
 import net.asec01.bcu.qingnian.picwall.Util.Companion.getFilesStorePath
 import net.coobird.thumbnailator.Thumbnails
 import net.coobird.thumbnailator.tasks.UnsupportedFormatException
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestHeader
@@ -16,6 +17,10 @@ import java.io.IOException
 @Controller
 @RequestMapping(value = ["/api"])
 class ApiController {
+
+    @Autowired
+    lateinit var apiService: ApiService
+
     @RequestMapping(value = ["/uploadImage.do"], produces = ["application/json; charset=utf-8"])
     @ResponseBody
     fun uploadImage(@RequestHeader headers: HttpHeaders, @RequestParam(value = "file") file: MultipartFile?): String? {
@@ -23,11 +28,13 @@ class ApiController {
         if (file == null) {
             return ResponseObject(2, "参数错误(0F)", null).toJson()
         }
+        val sessionId = Util.getCookie(headers)?.getValue("zzsession") ?: return ResponseObject(2, "参数错误(0S)", null).toJson()
+        val ua = headers["user-agent"]?.get(0) ?: return ResponseObject(2, "参数错误(0U)", null).toJson()
         //16 * 1024 * 1024 = 16777216
         if (file.size >= 16777216) {
             return ResponseObject(1, "图片大小不能超过16M", null).toJson()
         }
-        val saveFileName = Util.genFileName("upload", "test", saveFormat)
+        val saveFileName = Util.genFileName("upload", sessionId, saveFormat)
         val filePath = getFilesStorePath() + "images/"
         val oriFile: File = File.createTempFile(saveFileName,".tmp")
         try {
@@ -44,7 +51,6 @@ class ApiController {
             targetFile.getParentFile().mkdirs()
         }
         try {
-            return ResponseObject(0, null, ua).toJson()
             Thumbnails.of(oriFile).size(512, 512).outputFormat(saveFormat).toFile(targetFile)
         } catch (e: UnsupportedFormatException){
             return ResponseObject(1, "图片处理错误(不支持的图片格式)", null).toJson()
@@ -52,6 +58,10 @@ class ApiController {
             e.printStackTrace()
             return ResponseObject(1, "图片处理错误(${e.message})", null).toJson()
         }
+
+        val picObject = PicObject(targetFile.name, sessionId, ua)
+        apiService.addNew(picObject)
+        return ResponseObject(0, null, targetFile.name).toJson()
     }
 
     @RequestMapping(value = ["/getAll.do"], produces = ["application/json; charset=utf-8"])
