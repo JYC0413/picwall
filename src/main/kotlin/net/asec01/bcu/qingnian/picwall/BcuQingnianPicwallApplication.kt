23c6a47f9a5ab259149c6ba28ea1491347847110
Zhe Zhang <zhangzhe0177@gmail.com>
2020-12-20 14:23:48 +0800

    Initial commit

---
+++ src/main/kotlin/net/asec01/bcu/qingnian/picwall/BcuQingnianPicwallApplication.kt
@@ -0,0 +1,11 @@
+package net.asec01.bcu.qingnian.picwall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BcuQingnianPicwallApplication
+
+fun main(args: Array<String>) {
+    runApplication<BcuQingnianPicwallApplication>(*args)
+}
---
+++ src/test/kotlin/net/asec01/bcu/qingnian/picwall/BcuQingnianPicwallApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.asec01.bcu.qingnian.picwall
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BcuQingnianPicwallApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
